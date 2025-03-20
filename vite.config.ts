8e62f23fd78bde2ad369c9be3d023f94f5273723
BvspSite <bvsppress@gmail.com>
2025-03-20 20:52:52 +0700

    Menambahkan favicon ke proyek

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Fabrilio/', // Tambahkan ini sesuai dengan nama repository
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
